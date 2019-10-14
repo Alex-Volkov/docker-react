b3a7e38ccf082f5d5390251243330f30105b0a52
Alex <alexx.public@gmail.com>
2019-10-14 18:31:59 +0200

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('a renders without crashing 1', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+

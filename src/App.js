e7e48ab7ad8d432f5b2a4e2d0f9279a4451ced10
Alex <alexx.public@gmail.com>
2019-10-14 22:14:25 +0200

    changed on feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-lorem ipsum, Hello there!
+123213123lorem ipsum, Hello there!
         </p>
         <a
           className="App-link"
